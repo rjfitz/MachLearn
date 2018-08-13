e1722deef8540aab8847890ac5e96e599ccc9d03
Riley Fitzgibbons <rileyfitzgibbons@gmail.com>
2018-08-12 18:34:06 -0600

    Adding data Parser to be built to extract Diabetes Data

---
+++ DataParser.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+def loadFile(pathToFile):
+	from os import listdir
+	for file in :
+		print(file)
+
+def seperationIntoArray(line):
+	pass
+
+loadFile("~/Personal/MachLearn/Data/Diabetes-Data")
